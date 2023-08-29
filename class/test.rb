8254b44114d2cedc1ebf39322cd5430c012d6255
Ichsan Sandypratama <ichsan2668@gmail.com>
2023-08-29 16:22:55 +0700

    add rental.rb, book.rb, classroom.rb and modified person.rb

--- class/test.rb
+++ class/test.rb
@@ -1,10 +1,39 @@
+require_relative 'student'
+require_relative 'classroom'
+require_relative 'book'
 require_relative 'person'
-require_relative 'capitalize_decorator'
-require_relative 'trimmer_decorator'
+require_relative 'rental'
 
-person = Person.new(22, 'maximilianuserectus')
-person.correct_name
-capitalized_person = CapitalizeDecorator.new(person)
-puts capitalized_person.correct_name
-capitalized_trimmed_person = TrimmerDecorator.new(capitalized_person)
-puts capitalized_trimmed_person.correct_name
+michael = Student.new(32, 'Michael')
+melissa = Student.new(23, 'Melissa')
+austin = Student.new(23, 'Austin')
+
+
+ruby_classroom = Classroom.new('Ruby Module')
+ruby_classroom.add_student(melissa)
+michael.classroom = ruby_classroom
+ruby_classroom.add_student(austin)
+
+puts "We have #{ruby_classroom.students.length} students \nIn #{ruby_classroom.label}"
+puts(ruby_classroom.students.map { |s| "- #{s.name}" })
+
+anna = Person.new(22, 'Anna')
+john = Person.new(42, 'John')
+silvester = Person.new(42, 'Silvester')
+
+harry_potter = Book.new('Harry Potter', 'JK Rowling')
+moby_dick = Book.new('Moby Dick', 'Herman Mervile')
+
+Rental.new(Date.today, harry_potter, anna)
+Rental.new(Date.today, harry_potter, john)
+Rental.new(Date.today, moby_dick, silvester)
+Rental.new(Date.today, moby_dick, john)
+Rental.new(Date.today, moby_dick, anna)
+
+puts "\n"
+
+puts "For #{moby_dick.title}\nWe have #{moby_dick.rentals.length} rentals"
+puts(moby_dick.rentals.map { |r| "- #{r.person.name} at #{r.date}" })
+puts "\n"
+puts "For #{harry_potter.title}\nWe have #{harry_potter.rentals.length} rentals"
+puts(harry_potter.rentals.map { |r| "- #{r.person.name} at #{r.date}" })
